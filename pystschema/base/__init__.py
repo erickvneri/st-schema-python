4d98ed5dabec382cf9c77f776dd1628aa4c54df4
erickvneri <erickv@bluetrailsoft.com>
2020-04-21 18:22:38 -0500

    Base manufacturer class

--- pystschema/base/__init__.py
+++ pystschema/base/__init__.py
@@ -1,5 +1,5 @@
 from .header import Header, HeadersSchema
 from .base_device import BaseDevice, DeviceSchema
 from .base_cookie import BaseDeviceCookie, DeviceCookieSchema
-from .manufacturer_info import ManufacturerInfo, ManufacturerSchema
+from .manufacturer_info import BaseManufacturer, ManufacturerSchema
 from .base_device_context import BaseDeviceContext, DeviceContextSchema
