a7281af056683ff61acd2e693d31cbda1a5290f1
erickvneri <erickv@bluetrailsoft.com>
2020-04-24 23:46:36 -0500

    refactor BaseDevice, tests

--- pystschema/base/__init__.py
+++ pystschema/base/__init__.py
@@ -1,5 +1,5 @@
-from .header import Header, HeadersSchema
-from .base_device import BaseDevice, BaseDeviceSchema
+from .base_device import BaseDevice
 from .device_cookie import BaseCookie, DeviceCookieSchema
 from .manufacturer_info import BaseManufacturer, ManufacturerSchema
 from .device_context import BaseContext, DeviceContextSchema
+from .header import Header, HeadersSchema
--- pystschema/base/base_device.py
+++ pystschema/base/base_device.py
@@ -1,17 +1,16 @@
-from marshmallow import Schema, fields
-from pystschema.base.device_cookie import DeviceCookieSchema, BaseCookie
+from pystschema.base.device_cookie import BaseCookie
 
 class BaseDevice:
     """The BaseDevice class is the basic SmartThings
     Device representation that will contain the main
-    identification attributes of a Cloud-to-Cloud
+    attributes for identifying a Cloud Integrated
     device:
         :::param external_device_id: device id for a
         third-party cloud.
         :::param friendly_name: Name set by the user.
         :::param device_handler_type: value referred
         at the main ST-Schema documentation:
-        https://smartthings.developer.samsung.com/docs//devices/smartthings-schema/device-handler-types.html
+        https://smartthings.developer.samsung.com/docs/devices/smartthings-schema/device-handler-types.html
         :::param device_cookie: DeviceCookie object.
         :::param unique_device_id"""
 
@@ -24,23 +23,3 @@ class BaseDevice:
         self.device_unique_id = device_unique_id
         self.device_cookie = BaseCookie(device_cookie)
         self.device_handler_type = device_handler_type
-
-
-class BaseDeviceSchema(Schema):
-    """The DeviceSchema class will handle the
-    serialization of the BaseDevice and Device
-    classes. It parses the snake cased attributes
-    to Camel Case attributes as the ST-Schema
-    documentation refers:
-
-        :::param external_device_id -> externalDeviceId
-        :::param friendly_name -> friendlyName
-        :::param device_handler_type -> deviceHandlerType
-        :::param device_cookie -> deviceCookie
-        :::param device_unique_id -> uniqueDeviceId"""
-
-    externalDeviceId = fields.Field(attribute='external_device_id', dump_only=True)
-    friendlyName = fields.Field(attribute='friendly_name', dump_only=True)
-    deviceUniqueId = fields.Field(attribute='device_unique_id', dump_only=True)
-    deviceCookie = fields.Nested(DeviceCookieSchema, attribute='device_cookie', dump_only=True)
-    deviceHandlerType = fields.Field(attribute='device_handler_type', dump_only=True)
