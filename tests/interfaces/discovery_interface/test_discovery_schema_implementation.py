bcff77ec8c0774285b94a7e456e1af7d3f7466a4
erickvneri <erickv@bluetrailsoft.com>
2020-05-02 20:39:23 -0500

    updated implementation tests

--- tests/interfaces/discovery_interface/test_discovery_schema_implementation.py
+++ tests/interfaces/discovery_interface/test_discovery_schema_implementation.py
@@ -4,31 +4,39 @@ from stschema.base import BaseCookie, DeviceContext, ManufacturerInfo
 from tests.fixtures import DiscoveryFixture
 
 
-class TestDiscoverySchema:
+class TestDiscoverySchema(object):
+    """This class guarantees the reliability of the
+    data and format of a Discovery's device information"""
+
     @pytest.fixture
     def schema(self):
         schema = DeviceSchema()
         yield schema
 
     @pytest.fixture
-    def schema_class(self):
-        yield DeviceSchema
-
-    @pytest.fixture
     def device_fixture(self):
         device = DiscoveryFixture()
         yield device
 
-    def test_class_documentation(self, schema_class):
-        assert schema_class.__doc__
-        assert len(schema_class.__doc__) != 0
+    def test_class_documentation(self, schema):
+        assert schema.__doc__
+        assert len(schema.__doc__) != 0
 
-    def test_nested_classes(self, device_fixture):
+    def test_discovery_schema_composition(self, schema):
+        assert schema
+        assert schema.fields['externalDeviceId']
+        assert schema.fields['friendlyName']
+        assert schema.fields['deviceUniqueId']
+        assert schema.fields['deviceCookie']
+        assert schema.fields['deviceHandlerType']
+        assert schema.fields['manufacturerInfo']
+
+    def test_instance_parent_classes(self, device_fixture):
         assert isinstance(device_fixture.device_cookie, BaseCookie)
         assert isinstance(device_fixture.manufacturer_info, ManufacturerInfo)
         assert isinstance(device_fixture.device_context, DeviceContext)
 
-    def test_device_representation_object(self, device_fixture):
+    def test_discovery_fixture_instance(self, device_fixture):
         assert device_fixture
         assert device_fixture.external_device_id
         assert device_fixture.device_cookie
@@ -37,13 +45,13 @@ class TestDiscoverySchema:
         assert device_fixture.device_unique_id
         assert device_fixture.manufacturer_info
 
-    def test_device_schema_dump(self, schema, device_fixture):
+    def test_discovery_fixture_schema_instance(self, schema, device_fixture):
         device_result = schema.dump(device_fixture)
         assert schema
-        assert device_result['externalDeviceId'] == device_fixture.external_device_id
-        assert device_result['friendlyName'] == device_fixture.friendly_name
-        assert device_result['deviceUniqueId'] == device_fixture.device_unique_id
-        assert device_result['deviceCookie'] == device_fixture.device_cookie.__dict__
-        assert device_result['deviceHandlerType'] == device_fixture.device_handler_type
+        assert device_result['externalDeviceId']
+        assert device_result['friendlyName']
+        assert device_result['deviceUniqueId']
+        assert device_result['deviceCookie']
+        assert device_result['deviceHandlerType']
         assert device_result['manufacturerInfo']
         assert device_result['deviceContext']
