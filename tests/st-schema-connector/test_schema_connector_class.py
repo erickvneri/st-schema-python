4500056497bb187bb9e423a1dcc0d25bccb2e2b1
erickvneri <erickv@bluetrailsoft.com>
2020-05-10 01:22:24 -0500

    discovery-state handlers

--- tests/st-schema-connector/test_schema_connector_class.py
+++ tests/st-schema-connector/test_schema_connector_class.py
@@ -5,7 +5,6 @@ from stschema import SchemaConnector
 class TestSchemaConnector(object):
     @pytest.fixture
     def connector(self):
-
         yield SchemaConnector
 
     def test_schema_connector_documentation(self, connector):
@@ -13,8 +12,22 @@ class TestSchemaConnector(object):
         assert connector.__doc__
         assert len(connector.__doc__) != 0
 
+    def test_schema_connector_type_error(self, connector):
+        with pytest.raises(TypeError):
+            assert connector(credentials='cred:pass')
+            assert connector(password='12345')
+            assert connector(secret='secret')
+            assert connector(client_secret='1-2aSbs_sd')
+
     def test_schema_connector_composition(self, connector):
         assert connector
         assert connector.discovery_handler
         assert connector.state_refresh_handler
         assert connector.command_handler
+
+    def test_attribute_error_composition_schema_connector(self, connector):
+        with pytest.raises(AttributeError):
+            assert connector.not_supported_method
+            assert connector.get_credentials
+            assert connector.get_free_token
+            assert connector.password
