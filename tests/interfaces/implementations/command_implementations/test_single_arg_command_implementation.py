055ad24757c04ece04e4cdd980478187ec57921c
erickvneri <erickv@bluetrailsoft.com>
2020-05-15 00:27:38 -0500

    tests on single cmd handler

--- tests/interfaces/implementations/command_implementations/test_single_arg_command_implementation.py
+++ tests/interfaces/implementations/command_implementations/test_single_arg_command_implementation.py
@@ -1,21 +1,21 @@
 import pytest
-from stschema.interface import CommandHandler
 from stschema.base.device import BaseState
+from stschema.interface import CommandHandler
 
 
-class TestVoidCommand(object):
+class TestSingleArgCommand(object):
     @pytest.fixture
-    def void_cmd(self):
+    def single_arg_command(self):
         cmd = dict(
             component='main',
-            capability='st.doorControl',
-            command='open',
-            arguments=[]
+            capability='st.thermostatCoolingSetpoint',
+            command='setCoolingSetpoint',
+            arguments=[69]
         )
         yield cmd
 
-    def test_result_instance(self, void_cmd):
-        handled_cmd = CommandHandler(void_cmd).get_state()
+    def test_result_instance(self, single_arg_command):
+        handled_cmd = CommandHandler(single_arg_command).get_state()
         assert isinstance(handled_cmd, BaseState)
         assert handled_cmd
         assert handled_cmd.component
@@ -24,10 +24,12 @@ class TestVoidCommand(object):
         assert handled_cmd.value
         assert handled_cmd.unit or handled_cmd.unit is None
 
-    def test_value_error_capability(self, void_cmd):
+    def test_value_error_capability(self, single_arg_command):
         """ValueError exception raised if
         Schema Connector receives capability
         without "st." prefix"""
-        void_cmd['capability'] = void_cmd['capability'].lstrip('st.')
+        single_arg_command['capability'] = single_arg_command['capability'].lstrip('st.')
         with pytest.raises(ValueError):
-            handled_cmd = CommandHandler(void_cmd).get_state()
+            handled_cmd = CommandHandler(single_arg_command).get_state()
+
+
