9d81c6030562db2ed66027ceb6eebc38328bdaf2
erickvneri <erickv@bluetrailsoft.com>
2020-05-17 16:10:39 -0500

    command handler

--- stschema/schema_connector.py
+++ stschema/schema_connector.py
@@ -1,5 +1,6 @@
 from typing import List
-from stschema.interface import Device
+from stschema.interface import Device, CommandHandler
+from stschema.interface.schemas import DeviceCommandMapper
 from stschema.responses import (DiscoveryResponse, StateResponse,
                                 DiscoveryResponseSchema, StateRefreshResponseSchema)
 
@@ -25,28 +26,48 @@ class SchemaConnector(object):
         the SchemaConnector will instance a custom logger
         to improve the debugging experience."""
 
-    # TODO: Implement actions of SchemaConnector.
+    command_mapper = DeviceCommandMapper()
+    state_response = StateRefreshResponseSchema()
+
+    # TODO: TBD SchemaConnector attributes.
     def __init__(
             self, devices: List[Device], save_access_code: bool = False, logging: bool = False
     ):
         self.devices = devices
 
     def discovery_handler(self, request_id: str):
+        """Returns discoveryResponse JSON according to
+        the SmartThings Schema Connector documentation"""
         response = DiscoveryResponse(devices=self.devices, request_id=request_id)
         discovery_schema = DiscoveryResponseSchema()
         return discovery_schema.dump(response)
 
     def state_refresh_handler(self, request_id: str):
         response = StateResponse(devices=self.devices, request_id=request_id)
+        """Returns stateRefreshResponse JSON according to
+        the SmartThings Schema Connector documentation"""
         state_response = StateRefreshResponseSchema()
         return state_response.dump(response)
 
-    @staticmethod
-    def command_handler(command_device: List, request_id: str):
-        """
-        1. Filter device.
-        2. Map Command - Get State.
-        3. Instance StateResponse.
-        4. Dump new state.
-        """
-        pass
+    def command_handler(self, command_device: List, request_id: str):
+        """Returns commandResponse JSON according to
+        the SmartThings Schema Connector documentation"""
+        # DeviceCommandMapper Schema mapping Command payload.
+        command_req = self.command_mapper.load(command_device)
+
+        # Filter device by external_device_id
+        device = list(filter(lambda d: command_req['external_device_id'] == d.external_device_id, self.devices)).pop()
+
+        # CommandHandler to generate new state.
+        handled_cmd = CommandHandler(command_req['commands'].pop()).get_state()
+
+        # Clean states repository
+        device.states = []
+        if type(handled_cmd) is list:
+            device.states = [state for state in handled_cmd]
+        else:
+            device.states.append(handled_cmd)
+
+        # Instantiate StateResponse to handle command response
+        response = StateResponse(devices=[device], request_id=request_id, interaction_type='commandResponse')
+        return self.state_response.dump(response)
