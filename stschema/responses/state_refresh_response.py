cb3bd0c10ec59fe581fa9895e643ecd2071b46bd
erickvneri <erickv@bluetrailsoft.com>
2020-05-18 00:57:28 -0500

    pydoc responses

--- stschema/responses/state_refresh_response.py
+++ stschema/responses/state_refresh_response.py
@@ -6,13 +6,11 @@ from stschema.base.response import BaseResponse, HeadersSchema
 
 class StateResponse(BaseResponse):
     """Inherits from the BaseResponse class.
-    The StateResponse class handles the final
-    representation of a State Refresh Response
-    as the ST Schema documentation refers:
+    The StateResponse class handles the
+    representation of the stateRefreshResponse.
 
-        :::param device_state: a list of devices
-        specifying the current state of each capability.
-        :::param headers: headers or a request and response"""
+        :::param headers"
+        :::param device_state"""
 
     def __init__(self, devices: List[Device], request_id: str, interaction_type: str = 'stateRefreshResponse'):
         BaseResponse.__init__(self, interaction_type=interaction_type, request_id=request_id)
@@ -20,18 +18,11 @@ class StateResponse(BaseResponse):
 
 
 class StateRefreshResponseSchema(Schema):
-    """The StateRefreshResponseSchema will return
-    a StateRefresh Response JSON as it is referred
-    at the ST Schema documentation.
-
-        :::param deviceState:
-            [{
-                externalDeviceId,
-                deviceCookie
-                states: [{state}...]
-            }]
-        :::param headers: dict with JSON
-        headers inherited from ST response."""
+    """The StateRefreshResponseSchema handles
+    the serialization of the StateResponse class.
+    It converts Snake Case attributes to
+    Camel Case format following the REST
+    conventions."""
 
     headers = fields.Nested(HeadersSchema, attribute='headers')
     deviceState = fields.List(fields.Nested(DeviceStateSchema), attribute='device_state')
