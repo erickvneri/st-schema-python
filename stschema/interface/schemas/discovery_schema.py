8143435dec79fa5179f9de22d65862ca4a048a8d
erickvneri <erickv@bluetrailsoft.com>
2020-05-18 00:56:01 -0500

    pydoc - interface

--- stschema/interface/schemas/discovery_schema.py
+++ stschema/interface/schemas/discovery_schema.py
@@ -4,19 +4,13 @@ from stschema.base.util import DeviceCookieSchema
 
 
 class DeviceDiscoverySchema(Schema):
-    """The DiscoverySchema returns the formal
-    representation of a device in a Discovery
-    Response.
+    """The DeviceDiscoverySchema handles the
+    serialization of the Device interface."""
 
-    It handles the keys for the next Nested schemas:
-        - DeviceCookieSchema
-        - ManufacturerSchema
-        - DeviceContextSchema"""
-
-    externalDeviceId = fields.Field(attribute='external_device_id', dump_only=True)
-    friendlyName = fields.Field(attribute='friendly_name', dump_only=True)
-    deviceHandlerType = fields.Field(attribute='device_handler_type', dump_only=True)
-    deviceUniqueId = fields.Field(attribute='device_unique_id', dump_only=True)
+    externalDeviceId = fields.Field(attribute='external_device_id')
+    friendlyName = fields.Field(attribute='friendly_name')
+    deviceHandlerType = fields.Field(attribute='device_handler_type')
+    deviceUniqueId = fields.Field(attribute='device_unique_id')
     deviceCookie = fields.Nested(DeviceCookieSchema, attribute='device_cookie')
     manufacturerInfo = fields.Nested(ManufacturerSchema, attribute='manufacturer_info')
     deviceContext = fields.Nested(DeviceContextSchema, attribute='device_context')
