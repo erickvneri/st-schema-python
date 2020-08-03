83eafbdd416792163a206e0fba9ac0fe3ea8b1be
erickvneri <erickv@bluetrailsoft.com>
2020-08-02 22:24:58 -0500

    event_logger module

---
+++ stschema/util/event_logger/event_logger.py
@@ -0,0 +1,22 @@
+import logging
+from colorama import Fore
+
+
+class EventLogger:
+    def __init__(self, enable: bool):
+        self.logger = self._logger(enable)
+
+    def _logger(self, enable):
+        logger = logging.getLogger('[stschema]')
+        # Handle levels
+        logger.setLevel(logging.WARNING)
+        if enable:
+            logger.setLevel(logging.DEBUG)
+
+        #  Stream handler instance.
+        stream  = logging.StreamHandler()
+        # Formatter instance
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+        stream.setFormatter(formatter)
+        logger.addHandler(stream)
+        return logger
