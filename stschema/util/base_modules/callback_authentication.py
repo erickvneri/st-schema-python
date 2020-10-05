3aca01ef0904f00957f22166afd0ade1d53ef72b
erickvneri <erickv@bluetrailsoft.com>
2020-10-05 00:36:30 -0500

    RF Token - Schema flow control

--- stschema/util/base_modules/callback_authentication.py
+++ stschema/util/base_modules/callback_authentication.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, pre_dump
 
 
 class CallbackAuthentication:
@@ -12,10 +12,11 @@ class CallbackAuthentication:
         - State Callback.
         - Discovery Callback.
     """
-    def __init__(self, client_id: str, client_secret: str, code: str, grant_type: str) -> 'CallbackAuthentication':
+    def __init__(self, client_id: str, client_secret: str, code: str, refresh_token: str, grant_type: str) -> 'CallbackAuthentication':
         self.client_id = client_id
         self.client_secret = client_secret
         self.code = code
+        self.refresh_token = refresh_token
         self.grant_type = grant_type
 
 
@@ -29,7 +30,18 @@ class CallbackAuthSchema(Schema):
     formatting conventions for JSON
     string objects.
     """
-    clientId = fields.Str(attribute='client_id', required=True, dump_only=True)
-    clientSecret = fields.Str(attribute='client_secret', required=True, dump_only=True)
-    code = fields.Str(attribute='code', required=True, dump_only=True)
-    grantType = fields.Str(attribute='grant_type', required=True, dump_only=True)
+
+    @pre_dump
+    def _verify_dump(self, data, **kwargs):
+        self.dump_fields.update(
+            clientId = fields.Str(attribute='client_id', required=True, dump_only=True),
+            clientSecret = fields.Str(attribute='client_secret', required=True, dump_only=True),
+            grantType = fields.Str(attribute='grant_type', required=True, dump_only=True))
+        if data.code:
+            self.dump_fields.update(
+                code = fields.Str(attribute='code', required=True, dump_only=True))
+        elif data.refresh_token:
+            self.dump_fields.update(
+                refreshToken = fields.Str(attribute='refresh_token', required=True, dump_only=True))
+        return data
+
