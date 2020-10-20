63f00e75162905c1ec07bd1825ce9c65702e5c96
erickvneri <erickv@bluetrailsoft.com>
2020-10-20 13:36:20 -0500

    removed colorama ref

--- stschema/util/error_handler/error_handler.py
+++ stschema/util/error_handler/error_handler.py
@@ -1,5 +1,3 @@
-from colorama import Fore
-
 class ErrorHandler:
     """
     ErrorHandler class to pretty print
@@ -12,5 +10,4 @@ class ErrorHandler:
 
     def not_implemented_error(self, origin: str):
         error_msg = '[%s] - Interaction resource handler not implemented' % origin
-        formatted_msg = Fore.RED + error_msg + Fore.RESET
-        raise NotImplementedError(formatted_msg)
+        raise NotImplementedError(error_msg)
