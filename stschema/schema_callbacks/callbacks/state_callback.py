8c61e370e77cb53ff395e57ec54193f9665d00ae
bardovasco <bardovasco@protonmail.com>
2021-08-10 22:55:43 -0500

    LICENCE update, black formatted

--- stschema/schema_callbacks/callbacks/state_callback.py
+++ stschema/schema_callbacks/callbacks/state_callback.py
@@ -1,5 +1,27 @@
+# MIT License
+#
+# Copyright (c) 2021 Erick Israel Vazquez Neri
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 from stschema.schema_response.responses import StateResponse
 from stschema.util.base_modules import BaseAuthentication
+
 
 class StateCallback(StateResponse):
     """
@@ -7,9 +29,14 @@ class StateCallback(StateResponse):
     the information used to instance
     Device State Callbacks.
     """
-    def __init__(self, access_token: str, request_id: str, devices: list) -> 'StateCallback':
-        StateResponse.__init__(self,
-                               devices=devices,
-                               request_id=request_id,
-                               interaction_type='stateCallback')
+
+    def __init__(
+        self, access_token: str, request_id: str, devices: list
+    ) -> "StateCallback":
+        StateResponse.__init__(
+            self,
+            devices=devices,
+            request_id=request_id,
+            interaction_type="stateCallback",
+        )
         self.authentication = BaseAuthentication(access_token)
